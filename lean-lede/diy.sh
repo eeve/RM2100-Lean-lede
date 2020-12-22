ccd50e954ca3f09fdc18e45d206d2bd62ac66a29
eeve <16770728+eeve@users.noreply.github.com>
2020-12-23 00:09:24 +0800

    test 1

--- lean-lede/diy.sh
+++ lean-lede/diy.sh
@@ -5,17 +5,17 @@
 
 mkdir -p package/custom
 cd package/custom
-git clone https://github.com/openwrt-develop/luci-theme-atmaterial
-git clone https://github.com/tty228/luci-app-serverchan
-git clone https://github.com/rufengsuixing/luci-app-adguardhome
-git clone -b lede https://github.com/pymumu/luci-app-smartdns
-git clone https://github.com/pymumu/openwrt-smartdns
-git clone -b 18.06  https://github.com/jerrykuku/luci-theme-argon
-git clone https://github.com/vernesong/OpenClash
-git clone https://github.com/Lienol/openwrt-package
-git clone https://github.com/fw876/helloworld
-git clone https://github.com/kenzok8/openwrt-packages
-git clone https://github.com/kenzok8/small
+git clone --depth=1 https://github.com/openwrt-develop/luci-theme-atmaterial
+git clone --depth=1 https://github.com/tty228/luci-app-serverchan
+git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome
+git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns
+git clone --depth=1 https://github.com/pymumu/openwrt-smartdns
+git clone --depth=1 -b 18.06  https://github.com/jerrykuku/luci-theme-argon
+git clone --depth=1 https://github.com/vernesong/OpenClash
+git clone --depth=1 https://github.com/Lienol/openwrt-package
+git clone --depth=1 https://github.com/fw876/helloworld
+git clone --depth=1 https://github.com/kenzok8/openwrt-packages
+git clone --depth=1 https://github.com/kenzok8/small
 
 cd ../../
 
@@ -30,4 +30,4 @@ sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
 # Modify some default settings
 
 #\cp -rf ../lede/zzz-default-settings package/lean/default-settings/files/zzz-default-settings
-curl -fsSL ../lean-lede/zzz-default-settings > package/lean/default-settings/files/zzz-default-settings
+cp -rf ../lean-lede/zzz-default-settings > package/lean/default-settings/files/zzz-default-settings
